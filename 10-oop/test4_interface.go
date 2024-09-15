51c5ba403b962605d3ddcbb7560175b827bc59ba
Tiez <115238388+ContinueLearner@users.noreply.github.com>
2024-09-15 14:38:10 +0800

    add Polymorphism

---
+++ 10-oop/test4_interface.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type AnimalIF interface {
+	Sleep()
+	GetColor() string
+	GetType() string
+}
+
+type Cat struct {
+	color string
+}
+
+func (this *Cat) Sleep() {
+	fmt.Println("Cat is Sleep")
+}
+
+func (this *Cat) GetColor() string {
+	return this.color
+}
+
+func (this *Cat) GetType() string {
+	return "Cat"
+}
+
+type Dog struct {
+	color string
+}
+
+func (this *Dog) Sleep() {
+	fmt.Println("Dog is Sleep")
+}
+
+func (this *Dog) GetColor() string {
+	return this.color
+}
+
+func (this *Dog) GetType() string {
+	return "Dog"
+}
+
+//多态
+func showAnimal(animal AnimalIF) {
+	animal.Sleep()
+	fmt.Println("color = ", animal.GetColor())
+	fmt.Println("kind = ", animal.GetType())
+}
+
+func main() {
+	cat := Cat{"Red"}
+	dog := Dog{"Green"}
+	showAnimal(&cat)
+	showAnimal(&dog)
+}
