7f466d6d1f3d0ffe59e642aba172b07d1bcb81b1
Tiez <115238388+ContinueLearner@users.noreply.github.com>
2024-09-26 15:05:36 +0800

    add user broadcast

---
+++ 14-golang-IM-system/ServerV0.3-用户消息广播/server.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//广播消息的channel
+	Message chan string
+}
+
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+
+	return server
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+func (this *Server) Handler(conn net.Conn) {
+	//...当前链接的业务
+	//fmt.Println("链接建立成功")
+
+	user := NewUser(conn)
+
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.BroadCast(user, "I am online")
+
+	//接收客户端发送的消息，然后进行广播
+	go func() {
+		buf := make([]byte, 4096)
+
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "offline")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("conn read err:", err)
+				return
+			}
+
+			msg := string(buf[:n-1])
+
+			this.BroadCast(user, msg)
+
+		}
+	}()
+
+	select {}
+}
+
+// 启动服务器的接口
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	//close listen socket
+	defer listener.Close()
+
+	//启动监听message的goroutine
+	go this.ListenMessager()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+
+		//do handler
+		go this.Handler(conn)
+	}
+}
